337b4b37384cad2ceae2c357a86c4ab5613b07cb
mehmetokur <77415001+mehmetokur@users.noreply.github.com>
2021-02-04 00:11:56 +0300

    03.02.2021 ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -22,5 +22,20 @@ namespace Business.Concrete
             //iş kodları
             return _productDal.GetAll();
         }
+
+        public IEnumerable<Product> GetByUnitPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max);
+        }
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class demek refereans tipi olabilir demekve Ientity olabilir ya da implemente eden olabilir
+    //new lenebilir olmalı
+   public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Uptade(T entity);
+        void Delete(T entity);
+    }
+}
